00f58819b19c86e55fa3a91c4cca2a8136e8401d
Andrew <andrewchan077@gmail.com>
2019-12-30 16:27:47 +1100

    1. First Component

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # github-battle-revision
+npm install --save-dev @babel/core @babel/preset-env @babel/preset-react webpack webpack-cli webpack-dev-server babel-loader css-loader style-loader html-webpack-plugin
---
+++ app/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+
+// Component
+// - State (manage their own)
+// - Lifecycle
+// - UI
+
+class App extends React.Component {
+    render() {
+       // Description of what the UI will look like
+       return (
+           <div>
+               Hello World!
+            </div>
+       )
+        // Babel is needed to convert this JSX code into normal Javascript (browser-readable code)
+        //    return React.createElement(
+        //        "div",
+        //        null,
+        //        "Hello World!"
+        //    )
+    }
+}
+
+// ReactDOM is de-coupled from the React package so that you can develop in React for other platforms to render to, not necessarily the DOM, e.g. phone apps
+ReactDOM.render(
+    // Takes two arguments:
+    // React Element
+    // Where to render the Element to
+    <App/>, // Remember JSX > Javascript via Babel creates a React Element
+    document.getElementById('app')
+)
