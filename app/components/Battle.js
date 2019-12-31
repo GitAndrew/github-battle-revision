e9c6be2fa51ee8b5823660149425c9e016cd3b27
Andrew <andrewchan077@gmail.com>
2019-12-31 17:04:33 +1100

    13. Results Skeleton

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { FaUserFriends, FaFighterJet, FaTrophy, FaTimesCircle, FaTimes } from 'react-icons/fa'
 import PropTypes from 'prop-types'
+import Results from './Results'
 
 function Instructions() {
     return (
@@ -125,7 +126,8 @@ export default class Battle extends React.Component {
 
         this.state = {
             playerOne: null,
-            playerTwo: null
+            playerTwo: null,
+            battle: false
         }
 
         this.handleSubmit = this.handleSubmit.bind(this)
@@ -145,7 +147,11 @@ export default class Battle extends React.Component {
     }
 
     render() {
-        const { playerOne, playerTwo } = this.state
+        const { playerOne, playerTwo, battle } = this.state
+
+        if (battle) {
+            return <Results playerOne={playerOne} playerTwo={playerTwo} />
+        }
 
         return (
             <React.Fragment>
@@ -177,6 +183,16 @@ export default class Battle extends React.Component {
                             />
                             }
                     </div>
+                    {playerOne && playerTwo && (
+                        <button
+                            className='btn btn-dark btn-space'
+                            onClick={() => this.setState({
+                                battle: true
+                            })}
+                        >
+                            Battle
+                        </button>
+                    )}
                 </div>
             </React.Fragment>
         )
